8d90aab09c42027fb567ccfa52eefb528d1f0a77
Laura Shamus <lkshamus@gmail.com>
2019-10-02 13:59:44 -0600

    Add new problem

--- leetcode.js
+++ leetcode.js
@@ -29,3 +29,19 @@ var defangIPaddr = function(address) {
 };
 
 defangIPaddr("1.1.1.1")
+
+// Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+// Input: arr = [1,2,2,1,1,3]
+// Output: true
+// Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+var uniqueOccurrences = function(arr) {
+  const freq = {};
+  for (const val of arr) {
+    freq[val] = (freq[val] || 0) + 1;
+  }
+   return Object.keys(freq).length === new Set(Object.values(freq)).size;
+}
+
+uniqueOccurrences([1,2,2,1,1,3,2])
